f9859dba0892b045dd02f9259d345de32145acb2
heeeaeee <heeheeaeaeae@gmail.com>
2019-04-03 17:04:35 +0900

    bundleTest

---
+++ src/main/java/org/gradle/Index.java
@@ -0,0 +1,13 @@
+package org.gradle;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Index {
+	public static void main(String[] args) {
+        SpringApplication.run(Index.class);
+    }
+
+}
